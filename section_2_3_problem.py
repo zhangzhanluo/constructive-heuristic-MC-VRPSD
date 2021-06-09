a54f225faf7c4628c5a443fa93cb24d3a9619200
zhanluo <zhangzhanluo@sjtu.edu.cn>
2021-06-09 10:26:53 +0800

    实现了SCW算法以及路线的可视化。

--- section_2_3_problem.py
+++ section_2_3_problem.py
@@ -1,7 +1,10 @@
+import os
 import math
 import random
 from functools import reduce
 import scipy.stats as stats
+import networkx as nx
+from matplotlib import pyplot as plt
 
 
 class MCVRPSDInstance:
@@ -32,8 +35,8 @@ class MCVRPSDInstance:
         self.products_capacity = None
         self.tightness_ratio = 10
 
-        # 长度相关的设定
         self.random_seed = random_seed
+        self.name = 'Customers {} - CV {} - Random_Seed {}'.format(self.n_customers, self.cv, self.random_seed)
         self.further_init()
 
     @staticmethod
@@ -41,14 +44,19 @@ class MCVRPSDInstance:
         return round(math.sqrt(sum([(position_1[i] - position_2[i]) ** 2 for i in range(len(position_1))])), 2)
 
     def further_init(self):
+        """
+        进一步的初始化。
+
+        :return: 无
+        """
         random.seed(self.random_seed)
         self.depot_customers_position = [(random.uniform(0, self.distributed_space[0]),
                                           random.uniform(0, self.distributed_space[0])) for _ in
                                          range(self.n_customers + 1)]
         self.distances = [
             [self.euclidean_distance(self.depot_customers_position[i], self.depot_customers_position[j])
-             for i in range(self.n_customers)]
-            for j in range(self.n_customers)
+             for i in range(self.n_customers + 1)]
+            for j in range(self.n_customers + 1)
         ]
         customers_distance = [x[1:] for x in self.distances[1:]]
         self.L = round(random.uniform(3, 4) * max([max(x) for x in customers_distance]), 2)
@@ -74,6 +82,15 @@ class MCVRPSDInstance:
         """
         return sum([self.distances[r[i]][r[i + 1]] for i in range(len(r) - 1)])
 
+    def calculate_routes_planned_length(self, R):
+        """
+        计算routes 总计划长度
+
+        :param R: 路线的集合
+        :return: 路线总计划长度
+        """
+        return round(sum([self.calculate_planned_length(r) for r in R]), 2)
+
     def calculate_customers_failure_probability(self, r):
         """
         第2个客户失败的概率等于从第0个客户到当前才失败的概率+从第1个客户到当前就失败的概率。
@@ -85,16 +102,17 @@ class MCVRPSDInstance:
         """
         Pr = [0 for _ in range(len(r) - 1)]  # 记录第j个点失败的概率
         Pr[0] = 1  # 边界条件，对于depot处就更新概率为1（失败的具体影响就是更新该点）
-        for i in range(1, len(Pr)):  # i -> 第i个顾客， 具体对应的编号为r[i]
+        for i in range(1, len(Pr)):  # i -> 第i个顾客， 具体对应的编号为r[i]，在0-index的情况下，要用r[i]-1
             pr_j = []  # 记录从第j个点更新后到当前失败的概率
             for j in range(i):  # 从depot开始，到达i-1客户，求不同点更新对应的情况
                 if j == i - 1:  # 上面提到了，对于第i个客户前面一个客户更新，是不一样的，这里区别主要还是无法计算公式的第一项（不存在随机性了）
                     pr_j_1 = 1  # 第一部分概率，即到了i-1也不发生失败的概率，由于上一次才更新，因此这部分不存在随机性
                     # 到了第i个客户，累积的容量仍能满足要求的概率
                     customer_v_i_products_success_rate = [
-                        stats.norm.cdf((self.products_capacity[p] - self.customers_products_demand_mean[r[i]][p]) / (
-                                self.cv * self.customers_products_demand_mean[r[i]][p])
-                                       ) for p in range(self.n_products)]
+                        stats.norm.cdf(
+                            (self.products_capacity[p] - self.customers_products_demand_mean[r[i] - 1][p]) / (
+                                    self.cv * self.customers_products_demand_mean[r[i] - 1][p])
+                        ) for p in range(self.n_products)]
                     customer_v_i_success_rate = reduce(lambda x, y: x * y, customer_v_i_products_success_rate)
                     pr_j.append(pr_j_1 - customer_v_i_success_rate)
                 else:
@@ -102,12 +120,13 @@ class MCVRPSDInstance:
                     for m in [i, i + 1]:
                         # 先求每种产品累积的期望需求
                         m_accumulate_demand = [sum(
-                            [self.customers_products_demand_mean[r[k]][p] for k in range(j + 1, m)]) for p in
+                            [self.customers_products_demand_mean[r[k] - 1][p] for k in range(j + 1, m)]) for p in
                             range(self.n_products)]
                         # 再求每种产品累积的随机性（标准差）
-                        m_accumulate_std = [math.sqrt(sum([(self.customers_products_demand_mean[r[k]][p] * self.cv) ** 2
-                                                           for k in range(j + 1, m)]))
-                                            for p in range(self.n_products)]
+                        m_accumulate_std = [
+                            math.sqrt(sum([(self.customers_products_demand_mean[r[k] - 1][p] * self.cv) ** 2
+                                           for k in range(j + 1, m)]))
+                            for p in range(self.n_products)]
                         # 再求累积下来的成功率
                         m_accumulate_success_rate = [
                             stats.norm.cdf((self.products_capacity[p] - m_accumulate_demand[p]) / (
@@ -132,6 +151,15 @@ class MCVRPSDInstance:
         return round(self.calculate_planned_length(r) + sum(
             [self.distances[r[i + 1]][0] * 2 * Pr[i] for i in range(0, len(r) - 2)]), 2)
 
+    def calculate_routes_total_expected_length(self, R):
+        """
+        计算路线集合的总期望长度（成本）
+
+        :param R: 路线集合
+        :return: 路线集合的总期望长度（成本）
+        """
+        return round(sum([self.calculate_total_expected_length(r) for r in R]), 3)
+
     def check_distance_constrain(self, r):
         """
         检查路径的期望总长度是否小于L。
@@ -141,11 +169,56 @@ class MCVRPSDInstance:
         """
         return self.calculate_total_expected_length(r) <= self.L
 
+    def draw_routes(self, R, description=None, show_pic=True, save_pic_suffix=None):
+        """
+        对结果进行可视化。
+
+        :param R: 路线或路线的集合
+        :param description: 一些描述性的文字，会被打印在左上角
+        :param show_pic: show
+        :param save_pic_suffix: 保存图片名城的后缀，None的话不会保存图片
+        :return: 无
+        """
+        if R[0] is int:
+            R = [R]
+        g = nx.DiGraph()
+        g.add_nodes_from(range(self.n_customers + 1))
+        for node in g.nodes:
+            if node == 0:
+                g.nodes[node]['label'] = 'depot'
+                g.nodes[node]['demand'] = self.products_capacity
+            else:
+                g.nodes[node]['label'] = str(node)
+                g.nodes[node]['demand'] = self.customers_products_demand_mean[node - 1]
+        for r in R:
+            for i in range(len(r) - 1):
+                g.add_edge(r[i], r[i + 1], weight=self.distances[r[i]][r[i + 1]])
+        pos = {i: self.depot_customers_position[i] for i in g.nodes}
+        plt.figure(figsize=[7, 7])
+        nx.draw(g, pos, with_labels=True, font_color='w')
+        plt.text(1, 0, 'https://github.com/zhangzhanluo/constructive-heuristic-MC-VRPSD', ha='right', va='bottom',
+                 fontsize=6, transform=plt.gca().transAxes)
+        plt.text(0.01, 0.99, self.name, ha='left', va='top', fontsize=8, transform=plt.gca().transAxes)
+        if description is not None:
+            plt.text(0.01, 0.95, description, ha='left', va='top', fontsize=8, transform=plt.gca().transAxes)
+        if save_pic_suffix is not None:
+            pic_path = '01_Results/Pics/'
+            if not os.path.exists(pic_path):
+                os.makedirs(pic_path)
+            plt.savefig(
+                pic_path + '{} - {}.png'.format(self.name, save_pic_suffix if save_pic_suffix is not None else ''),
+                dpi=300)
+        if show_pic:
+            plt.show()
+        else:
+            plt.close()
+
 
 if __name__ == '__main__':
-    instance = MCVRPSDInstance(n_customers=25, random_seed=None)
+    instance = MCVRPSDInstance(n_customers=20, random_seed=0)
     print(instance.calculate_customers_failure_probability([0, 2, 4, 0]))
     print(instance.calculate_total_expected_length([0, 2, 4, 0]))
+    instance.draw_routes([[0, 1, 0], [0, 3, 4, 5, 6, 0]], save_pic_suffix='test', description='test')
 
     # 检验结果
     # 对于第一个客户(2号）而言，三种产品满足要求的概率分别是
--- section_4_SCW_heuristic.py
+++ section_4_SCW_heuristic.py
@@ -1,10 +1,24 @@
 from section_2_3_problem import MCVRPSDInstance
 
 
-def SCW(mcvrpsd_instance: MCVRPSDInstance):
+def SCW(instance: MCVRPSDInstance):
+    """
+    Similarly to the classical savings algorithm, the SCW heuristic starts from a trivial
+    solution comprised of n round trips from the depot to each customer. Then it tries at each iteration to
+    merge two routes by their extreme vertices (those connected to the depot), aiming to generate the largest
+    possible savings in the overall cost.
+
+    :param instance: 案例
+    :return: 规划的路线的集合
+    """
     R = []
-    for customer in range(1, mcvrpsd_instance.n_customers):
+    for customer in range(1, instance.n_customers + 1):
         R.append([0, customer, 0])
+    R_version = 0
+    instance.draw_routes(R, description='Planned Cost: {}\nTotal Expected Cost: {}'.format(
+        instance.calculate_routes_planned_length(R), instance.calculate_routes_total_expected_length(R)),
+                         show_pic=False, save_pic_suffix='SCW {}'.format(R_version))
+    R_version += 1
     while True:
         saving_list = []
         route_records = []
@@ -25,14 +39,14 @@ def SCW(mcvrpsd_instance: MCVRPSDInstance):
                                                   [e_u + v_s_apo, s_v_apo + u_e],
                                                   [s_v + u_e_apo, e_u_apo + v_s],
                                                   [s_v + v_s_apo, s_v_apo + v_s]]
-                original_cost = mcvrpsd_instance.calculate_total_expected_length(
-                    r) + mcvrpsd_instance.calculate_planned_length(r_apo)
+                original_cost = instance.calculate_total_expected_length(
+                    r) + instance.calculate_planned_length(r_apo)
                 for combination_pair in all_possible_combination_pairs:
                     combination_pairs_total_cost = [
-                        mcvrpsd_instance.calculate_total_expected_length(combination_pair[0]),
-                        mcvrpsd_instance.calculate_total_expected_length(combination_pair[1])]
+                        instance.calculate_total_expected_length(combination_pair[0]),
+                        instance.calculate_total_expected_length(combination_pair[1])]
                     min_merging_cost = min(combination_pairs_total_cost)
-                    if min_merging_cost < mcvrpsd_instance.L:
+                    if min_merging_cost < instance.L:
                         saving_list.append(original_cost - min_merging_cost)
                         route_records.append([r, r_apo])
                         combination_list.append(combination_pair[combination_pairs_total_cost.index(min_merging_cost)])
@@ -44,8 +58,12 @@ def SCW(mcvrpsd_instance: MCVRPSDInstance):
             R.remove(route_records[best_merge_index][0])
             R.remove(route_records[best_merge_index][1])
             R.append(combination_list[best_merge_index])
+            instance.draw_routes(R, description='Planned Cost: {}\nTotal Expected Cost: {}'.format(
+                instance.calculate_routes_planned_length(R), instance.calculate_routes_total_expected_length(R)),
+                                 show_pic=False, save_pic_suffix='SCW {}'.format(R_version))
+            R_version += 1
 
 
 if __name__ == '__main__':
-    instance = MCVRPSDInstance(n_customers=20, random_seed=0)
-    print(SCW(instance))
+    mcvrpsd_instance = MCVRPSDInstance(n_customers=20, random_seed=0)
+    print(SCW(mcvrpsd_instance))
